be4b70fe0b98c4ba4a8ca638994e39b4e02cb57c
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-08-14 03:47:59 +0300

    Update InfoState.hx

--- source/InfoState.hx
+++ source/InfoState.hx
@@ -44,7 +44,7 @@ class InfoState extends MusicBeatState
 		
                 var txt:FlxText = new FlxText(0, 0, FlxG.width,
 			"DISCLAIMER!"
-			+ "\n\nIf you're a Content Creator, then you should maybe skip Revenge since its Copyrighted!\nThere's a small WHITE FLASH on Suit Up but should'nt be too bad!\n\nThis Mod is still not Done!!\n\nTouch Your Screen Or Press Enter to proceed"
+			+ "\n\nIf you're a Content Creator, then you should maybe skip Revenge since its Copyrighted!\nThere's a small WHITE FLASH on Suit Up but should'nt be too bad!\n\nThis Mod is still not Done!!\n\n#id android Touch Your Screen Or #end Press Enter to proceed"
 
 			);
 		
