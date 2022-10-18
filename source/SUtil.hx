e7c8732ba1499a41cb49e64e15bf47014db9d991
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-10-18 20:54:16 +0300

    fucking susutil

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,17 +1,16 @@
 package;
 
 #if android
-import android.Hardware;
 import android.Permissions;
 import android.os.Build;
 import android.os.Environment;
+import android.widget.Toast;
 #end
 import flash.system.System;
 import flixel.FlxG;
 import haxe.CallStack.StackItem;
 import haxe.CallStack;
 import haxe.io.Path;
-import lime.app.Application;
 import openfl.Lib;
 import openfl.events.UncaughtErrorEvent;
 import openfl.utils.Assets;
@@ -35,7 +34,7 @@ class SUtil
 	{
 		#if android
 		if (!Permissions.getGrantedPermissions().contains(Permissions.WRITE_EXTERNAL_STORAGE)
-		       && !Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
+			&& !Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
 		{
 			if (VERSION.SDK_INT >= VERSION_CODES.M)
 			{
@@ -99,6 +98,7 @@ class SUtil
 
 			errMsg += u.error;
 
+			#if (sys && !ios)
 			try
 			{
 				if (!FileSystem.exists(SUtil.getPath() + 'logs'))
@@ -106,7 +106,7 @@ class SUtil
 
 				File.saveContent(SUtil.getPath()
 					+ 'logs/'
-					+ Application.current.meta.get('file')
+					+ Lib.application.meta.get('file')
 					+ '-'
 					+ Date.now().toString().replace(' ', '-').replace(':', "'")
 					+ '.log',
@@ -115,47 +115,23 @@ class SUtil
 			}
 			#if android
 			catch (e:Dynamic)
-			Hardware.toast("Error!\nClouldn't save the crash dump because:\n" + e, ToastType.LENGTH_LONG);
+			Toast.makeText("Error!\nClouldn't save the crash dump because:\n" + e, Toast.LENGTH_LONG);
+			#end
 			#end
 
-			haxe.Log.trace(errMsg, null);
-			Application.current.window.alert(errMsg, 'Error!');
-
+			println(errMsg);
+			Lib.application.window.alert(errMsg, 'Error!');
 			System.exit(1);
 		});
 	}
 
-        #if (sys && !ios)
-	public static function saveContent(fileName:String = 'file', fileExtension:String = '.json',
-			fileData:String = 'you forgot to add something in your code lol')
+	private static function println(msg:String):Void
 	{
-		try
-		{
-			if (!FileSystem.exists(SUtil.getPath() + 'saves'))
-				FileSystem.createDirectory(SUtil.getPath() + 'saves');
-
-			File.saveContent(SUtil.getPath() + 'saves/' + fileName + fileExtension, fileData);
-			#if android
-			Hardware.toast("File Saved Successfully!", ToastType.LENGTH_LONG);
-			#end
-		}
-		#if android
-		catch (e:Dynamic)
-		Hardware.toast("Error!\nClouldn't save the file because:\n" + e, ToastType.LENGTH_LONG);
+		#if sys
+		Sys.println(msg);
+		#else
+		// Pass null to exclude the position.
+		haxe.Log.trace(msg, null);
 		#end
 	}
-
-	public static function copyContent(copyPath:String, savePath:String)
-	{
-		try
-		{
-			if (!FileSystem.exists(savePath) && Assets.exists(copyPath))
-				File.saveBytes(savePath, Assets.getBytes(copyPath));
-		}
-		#if android
-		catch (e:Dynamic)
-		Hardware.toast("Error!\nClouldn't copy the file because:\n" + e, ToastType.LENGTH_LONG);
-		#end
-	}
-	#end
 }
