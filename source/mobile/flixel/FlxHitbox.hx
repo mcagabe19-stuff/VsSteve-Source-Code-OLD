094a48fb9bdd572ad8b64a5b1d4d9ed4b20b373b
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-10-23 11:28:43 +0300

    Revert FlxHitbox.hx

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -70,8 +70,8 @@ class FlxHitbox extends FlxSpriteGroup
 		hint.alpha = 0.00001;
 		hint.onDown.callback = function()
 		{
-			if (hint.alpha != 0.2)
-				hint.alpha = 0.2;
+			if (hint.alpha != MobileControls.getOpacity(true))
+				hint.alpha = MobileControls.getOpacity(true);
 		}
 		hint.onUp.callback = function()
 		{
