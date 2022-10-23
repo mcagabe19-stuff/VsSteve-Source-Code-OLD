18e2c6557999f163984aa211feb8447887bbb7d5
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-10-23 16:11:28 +0300

    :trollface:

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -270,15 +270,25 @@ class FreeplayState extends MusicBeatState
 
                 if(instPlaying != curSelected) {
                 if (#if mobile virtualPad.buttonC.justPressed || #end FlxG.keys.justPressed.SPACE) {
-                destroyFreeplayVocals();
-                vocals = new FlxSound().loadEmbedded(Paths.voices(songs[curSelected].songName), 0);
-                FlxG.sound.list.add(vocals);
-		FlxG.sound.playMusic(Paths.inst(songs[curSelected].songName), 0);
-                vocals.play();
+                #if PRELOAD_ALL
+		destroyFreeplayVocals();
+		FlxG.sound.music.volume = 0;
+		Paths.currentModDirectory = songs[curSelected].folder;
+		var poop:String = Highscore.formatSong(songs[curSelected].songName.toLowerCase(), curDifficulty);
+		PlayState.SONG = Song.loadFromJson(poop, songs[curSelected].songName.toLowerCase());
+		if (PlayState.SONG.needsVoices)
+			vocals = new FlxSound().loadEmbedded(Paths.voices(PlayState.SONG.song));
+		else
+		        vocals = new FlxSound();
+
+		FlxG.sound.list.add(vocals);
+		FlxG.sound.playMusic(Paths.inst(PlayState.SONG.song), 0.7);
+		vocals.play();
 		vocals.persist = true;
 		vocals.looped = true;
 		vocals.volume = 0.7;
-                instPlaying = curSelected;
+		instPlaying = curSelected;
+		#end
                 }
                 }
 
