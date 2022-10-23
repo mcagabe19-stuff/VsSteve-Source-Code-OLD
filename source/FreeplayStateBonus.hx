648e49821af6e1a657b36b24a15ad766d20881e4
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-10-23 13:21:18 +0300

    Update FreeplayStateBonus.hx

--- source/FreeplayStateBonus.hx
+++ source/FreeplayStateBonus.hx
@@ -9,8 +9,6 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import lime.utils.Assets;
-
-
 #if windows
 import Discord.DiscordClient;
 #end
@@ -37,6 +35,9 @@ class FreeplayStateBonus extends MusicBeatState
 
 	override function create()
 	{
+                Paths.clearUnusedMemory();
+                Paths.clearStoredMemory();
+
 		var initSonglist = CoolUtil.coolTextFile(Paths.txt('freeplayBonus'));
 
 		for (i in 0...initSonglist.length)
