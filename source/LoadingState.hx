d4b59de24a654d86e529919b781de7d96415c7fe
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-10-29 15:52:19 +0300

    stuff

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -1,305 +1,89 @@
 package;
 
-import lime.app.Promise;
-import lime.app.Future;
+import flixel.addons.transition.FlxTransitionableState;
 import flixel.FlxG;
 import flixel.FlxState;
-import flixel.FlxSprite;
-import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.FlxGraphic;
+import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import openfl.utils.Assets;
-import lime.utils.Assets as LimeAssets;
-import lime.utils.AssetLibrary;
-import lime.utils.AssetManifest;
+import sys.thread.Thread;
+import sys.FileSystem;
+import SUtil;
 
-import haxe.io.Path;
+using StringTools;
 
 class LoadingState extends MusicBeatState
 {
-	inline static var MIN_TIME = 1.0;
-	
-	var target:FlxState;
-	var stopMusic = false;
-	var callbacks:MultiCallback;
-	
-	var pano:FlxSprite;
-        var panoclone:FlxSprite;
-	var logoBl:FlxSprite;
-	var danceLeft = false;
-	
-	function new(target:FlxState, stopMusic:Bool)
+	public static var target:FlxState;
+	public static var stopMusic = false;
+	public var luaToCashe:Array<ModchartState> = [];
+	static var imagesToCache:Array<String> = [];
+	static var soundsToCache:Array<String> = [];
+	static var library:String = "";
+
+	var screen:LoadingScreen;
+
+	public function new()
 	{
 		super();
-		this.target = target;
-		this.stopMusic = stopMusic;
+
+		FlxTransitionableState.skipNextTransIn = true;
+		FlxTransitionableState.skipNextTransOut = true;
 	}
-	
+
 	override function create()
 	{
-		pano = new FlxSprite(-1600, 0).loadGraphic(Paths.image('titleBG'));
-		pano.antialiasing = true;
-		pano.updateHitbox();
-		add(pano);
+		super.create();
 
-		panoclone = new FlxSprite(-2880, 0).loadGraphic(Paths.image('titleBG'));
-		panoclone.antialiasing = true;
-		panoclone.updateHitbox();
-		add(panoclone);
+		// Disabled Now
+		/*switch (PlayState.SONG.song)
+		{
+			case 'suit up':
+				imagesToCache = [
+				]; }*/
 
-		logoBl = new FlxSprite(110, -0);
-		logoBl.frames = Paths.getSparrowAtlas('logoBumpinPreFinal');
-		logoBl.antialiasing = true;
-		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
-		logoBl.animation.play('bump');
-		logoBl.updateHitbox();
-		logoBl.y += 150;
-		logoBl.x += 200;
-		add(logoBl);
-		
-		initSongsManifest().onComplete
-		(
-			function (lib)
+		screen = new LoadingScreen();
+		add(screen);
+
+		screen.max = imagesToCache.length;
+
+		FlxG.camera.fade(FlxG.camera.bgColor, 0.5, true);
+
+		FlxGraphic.defaultPersist = true;
+		Thread.create(() ->
+		{
+			//#if !android
+			screen.setLoadingText("Loading images...");
+			for (image in imagesToCache)
 			{
-				callbacks = new MultiCallback(onLoad);
-				var introComplete = callbacks.add("introComplete");
-				checkLoadSong(getSongPath());
-				if (PlayState.SONG.needsVoices)
-					checkLoadSong(getVocalPath());
-				checkLibrary("shared");
-				/*if (PlayState.storyWeek > 0)
-					checkLibrary("week" + PlayState.storyWeek);
-				else
-					checkLibrary("tutorial");*/
-				
-				var fadeTime = 0.5;
-				FlxG.camera.fade(FlxG.camera.bgColor, fadeTime, true);
-				new FlxTimer().start(fadeTime + MIN_TIME, function(_) introComplete());
+				trace("Caching image " + image);
+				FlxG.bitmap.add(Paths.image(image));
+				screen.progress += 1;
 			}
-		);
-	}
-	
-	function checkLoadSong(path:String)
-	{
-		if (!Assets.cache.hasSound(path))
-		{
-			var library = Assets.getLibrary("songs");
-			final symbolPath = path.split(":").pop();
-			// @:privateAccess
-			// library.types.set(symbolPath, SOUND);
-			// @:privateAccess
-			// library.pathGroups.set(symbolPath, [library.__cacheBreak(symbolPath)]);
-			var callback = callbacks.add("song:" + path);
-			Assets.loadSound(path).onComplete(function (_) { callback(); });
-		}
-	}
-	
-	function checkLibrary(library:String)
-	{
-		trace(Assets.hasLibrary(library));
-		if (Assets.getLibrary(library) == null)
-		{
-			@:privateAccess
-			if (!LimeAssets.libraryPaths.exists(library))
-				throw "Missing library: " + library;
-			
-			var callback = callbacks.add("library:" + library);
-			Assets.loadLibrary(library).onComplete(function (_) { callback(); });
-		}
-	}
-	
-	override function beatHit()
-	{
-		super.beatHit();
-	}
-	
-	override function update(elapsed:Float)
-	{
-		super.update(elapsed);
-		#if debug
-		if (FlxG.keys.justPressed.SPACE)
-			trace('fired: ' + callbacks.getFired() + " unfired:" + callbacks.getUnfired());
-		#end
-	}
-	
-	function onLoad()
-	{
-		if (stopMusic && FlxG.sound.music != null)
-			FlxG.sound.music.stop();
-		
-		FlxG.switchState(target);
-	}
-	
-	static function getSongPath()
-	{
-		return Paths.inst(PlayState.SONG.song);
-	}
-	
-	static function getVocalPath()
-	{
-		return Paths.voices(PlayState.SONG.song);
-	}
-	
-	inline static public function loadAndSwitchState(target:FlxState, stopMusic = false)
-	{
-		FlxG.switchState(getNextState(target, stopMusic));
-	}
-	
-	static function getNextState(target:FlxState, stopMusic = false):FlxState
-	{
-		Paths.setCurrentLevel("week" + PlayState.storyWeek);
-		var loaded = isSoundLoaded(getSongPath())
-			&& (!PlayState.SONG.needsVoices || isSoundLoaded(getVocalPath()))
-			&& isLibraryLoaded("shared");
-		
-		if (!loaded)
-			return new LoadingState(target, stopMusic);
-		if (stopMusic && FlxG.sound.music != null)
-			FlxG.sound.music.stop();
-		
-		return target;
-	}
-	
-	static function isSoundLoaded(path:String):Bool
-	{
-		return Assets.cache.hasSound(path);
-	}
-	
-	static function isLibraryLoaded(library:String):Bool
-	{
-		return Assets.getLibrary(library) != null;
-        }
-	
-	override function destroy()
-	{
-                pano = null;
-                panoclone = null;
-                logoBl = null;
+			//#end
 
-		super.destroy();
-		
-		callbacks = null;
-	}
-	
-	static function initSongsManifest()
-	{
-		var id = "songs";
-		var promise = new Promise<AssetLibrary>();
+			FlxGraphic.defaultPersist = false;
 
-		var library = LimeAssets.getLibrary(id);
+			screen.setLoadingText("Done!");
+			trace("Done caching");
 
-		if (library != null)
-		{
-			return Future.withValue(library);
-		}
-
-		var path = id;
-		var rootPath = null;
-
-		@:privateAccess
-		var libraryPaths = LimeAssets.libraryPaths;
-		if (libraryPaths.exists(id))
-		{
-			path = libraryPaths[id];
-			rootPath = Path.directory(path);
-		}
-		else
-		{
-			if (StringTools.endsWith(path, ".bundle"))
+			FlxG.camera.fade(FlxColor.BLACK, 1, false);
+			new FlxTimer().start(1, function(_:FlxTimer)
 			{
-				rootPath = path;
-				path += "/library.json";
-			}
-			else
-			{
-				rootPath = Path.directory(path);
-			}
-			@:privateAccess
-			path = LimeAssets.__cacheBreak(path);
-		}
-
-		AssetManifest.loadFromFile(path, rootPath).onComplete(function(manifest)
-		{
-			if (manifest == null)
-			{
-				promise.error("Cannot parse asset manifest for library \"" + id + "\"");
-				return;
-			}
-
-			var library = AssetLibrary.fromManifest(manifest);
-
-			if (library == null)
-			{
-				promise.error("Cannot open library \"" + id + "\"");
-			}
-			else
-			{
-				@:privateAccess
-				LimeAssets.libraries.set(id, library);
-				library.onChange.add(LimeAssets.onChange.dispatch);
-				promise.completeWith(Future.withValue(library));
-			}
-		}).onError(function(_)
-		{
-			promise.error("There is no asset library with an ID of \"" + id + "\"");
+				screen.kill();
+				screen.destroy();
+				loadAndSwitchState(target, false);
+			});
 		});
+	}
 
-		return promise.future;
-	}
-}
+	public static function loadAndSwitchState(target:FlxState, stopMusic = false)
+	{
+		//Paths.setCurrentLevel("week" + PlayState.storyWeek);
 
-class MultiCallback
-{
-	public var callback:Void->Void;
-	public var logId:String = null;
-	public var length(default, null) = 0;
-	public var numRemaining(default, null) = 0;
-	
-	var unfired = new Map<String, Void->Void>();
-	var fired = new Array<String>();
-	
-	public function new (callback:Void->Void, logId:String = null)
-	{
-		this.callback = callback;
-		this.logId = logId;
+		if (stopMusic && FlxG.sound.music != null)
+			FlxG.sound.music.stop();
+
+		MusicBeatState.switchState(target);
 	}
-	
-	public function add(id = "untitled")
-	{
-		id = '$length:$id';
-		length++;
-		numRemaining++;
-		var func:Void->Void = null;
-		func = function ()
-		{
-			if (unfired.exists(id))
-			{
-				unfired.remove(id);
-				fired.push(id);
-				numRemaining--;
-				
-				if (logId != null)
-					log('fired $id, $numRemaining remaining');
-				
-				if (numRemaining == 0)
-				{
-					if (logId != null)
-						log('all callbacks fired');
-					callback();
-				}
-			}
-			else
-				log('already fired $id');
-		}
-		unfired[id] = func;
-		return func;
-	}
-	
-	inline function log(msg):Void
-	{
-		if (logId != null)
-			trace('$logId: $msg');
-	}
-	
-	public function getFired() return fired.copy();
-	public function getUnfired() return [for (id in unfired.keys()) id];
 }
