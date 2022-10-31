ee2874ce848a478c9092ca5979d9f2bc29e0714e
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-10-31 11:31:37 +0300

    Update Highscore.hx

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -16,7 +16,7 @@ class Highscore
 		var daSong:String = formatSong(song, diff);
 
 
-		#if !switch
+		#if newgrounds
 		NGio.postScore(score, song);
 		#end
 
@@ -35,7 +35,7 @@ class Highscore
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0):Void
 	{
 
-		#if !switch
+		#if newgrounds
 		NGio.postScore(score, "Week " + week);
 		#end
 
