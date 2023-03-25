8e6abbbac5640a0002bfee63887feb164d07875e
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-25 12:09:47 +0300

    Update OptionsMenu.hx

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -33,7 +33,7 @@ class OptionsMenu extends MusicBeatState
 			new FPSCapOption("Cap your FPS"),
 			new ScrollSpeedOption("Change your scroll speed (1 = Chart dependent)"),
 			new AccuracyDOption("Change how accuracy is calculated. (Accurate = Simple, Complex = Milisecond Based)"),
-			new ResetButtonOption("Toggle pressing R to gameover."),
+			#if !web new ResetButtonOption("Toggle pressing R to gameover."), #end
 			// new OffsetMenu("Get a note offset based off of your inputs!"),
 			//new CustomizeGameplay("Drag'n'Drop Gameplay Modules around to your preference")
 		]),
