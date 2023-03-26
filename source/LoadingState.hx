1630303ae22f9083cab8c2afd620b8b25b0df457
mcagabe19 <egzozu.be.bas@gmail.com>
2023-03-26 13:37:40 +0300

    test

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -1,63 +1,40 @@
 package;
 
-import lime.app.Promise;
-import lime.app.Future;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxG;
 import flixel.FlxState;
-import flixel.FlxSprite;
-import flixel.util.FlxColor;
-import flixel.text.FlxText;
 import flixel.graphics.FlxGraphic;
-import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import openfl.utils.Assets;
-import lime.utils.Assets as LimeAssets;
-import lime.utils.AssetLibrary;
-import lime.utils.AssetManifest;
-import flixel.addons.transition.FlxTransitionableState;
 import flixel.math.FlxMath;
-import haxe.io.Path;
+import flixel.text.FlxText;
+import openfl.utils.Assets;
+#if sys
+import sys.thread.Thread;
+import sys.FileSystem;
+#end
+//#if mobile
+//import SUtil;
+//#end
 
-class LoadingState extends MusicBeatState
+using StringTools;
+
+class LoadingScreen extends FlxTypedGroup<FlxSprite>
 {
-        public var progress:Int = 0;
-        public var max:Int = 10;
+	public var progress:Int = 0;
+	public var max:Int = 10;
 
-        static var imagesToCache:Array<String> = [];
-
-	inline static var MIN_TIME = 1.0;
-	
-	var target:FlxState;
-	var stopMusic = false;
-	var callbacks:MultiCallback;
-	
 	var pano:FlxSprite;
-        var panoclone:FlxSprite;
+	var panoclone:FlxSprite;
 	var logoBl:FlxSprite;
-        var loadTxtBg:FlxSprite;
+	var loadTxtBg:FlxSprite;
 	var loadTxtProgress:FlxSprite;
 	var loadTxt:FlxText;
-	var danceLeft = false;
-	
-	function new(target:FlxState, stopMusic:Bool)
+
+	public function new()
 	{
 		super();
-
-                FlxTransitionableState.skipNextTransIn = true;
-		FlxTransitionableState.skipNextTransOut = false;
-		this.target = target;
-		this.stopMusic = stopMusic;
-	}
-	
-	override function create()
-	{
-                switch (PlayState.SONG.song)
-		{
-			case 'bos':
-				imagesToCache = [
-					'KickedBG',
-				];
-                }
 
 		pano = new FlxSprite(-1600, 0).loadGraphic(Paths.image('titleBG'));
 		pano.antialiasing = true;
@@ -79,26 +56,26 @@ class LoadingState extends MusicBeatState
 		logoBl.x += 200;
 		add(logoBl);
 
-                loadTxtBg = new FlxSprite();
+		loadTxtBg = new FlxSprite();
 		add(loadTxtBg);
 
 		loadTxtProgress = new FlxSprite();
 		add(loadTxtProgress);
 
-		loadTxt = new FlxText(0, 24, 0, "Loading...", 30);
+		loadTxt = new FlxText(0, 0, 0, "Loading...", 30);
 		loadTxt.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		loadTxt.x = 5;
 		loadTxt.y = FlxG.height - loadTxt.height - 5;
 		add(loadTxt);
 
-		loadTxtBg.makeGraphic(1, 24, 0xFF000000);
+		loadTxtBg.makeGraphic(1, 1, 0xFF000000);
 		loadTxtBg.updateHitbox();
 		loadTxtBg.origin.set();
 		loadTxtBg.scale.set(1280, loadTxt.height + 5);
 		loadTxtBg.alpha = 0.8;
 		loadTxtBg.y = loadTxt.y;
 
-		loadTxtProgress.makeGraphic(1, 24, 0xFFFFFFFF);
+		loadTxtProgress.makeGraphic(1, 1, 0xFFFFFFFF);
 		loadTxtProgress.updateHitbox();
 		loadTxtProgress.origin.set();
 		loadTxtProgress.scale.set(0, loadTxt.height + 5);
@@ -106,84 +83,74 @@ class LoadingState extends MusicBeatState
 		loadTxtProgress.y = loadTxt.y;
 
 		loadTxt.y += 2;
+	}
 
-                max = imagesToCache.length;
-
-                FlxGraphic.defaultPersist = true;
-		
-		initSongsManifest().onComplete
-		(
-			function (lib)
-			{
-                                for (image in imagesToCache) {
-				trace("Caching image " + image);
-				progress += 1;
-                                FlxG.bitmap.add(Paths.image(image)); }
-				callbacks = new MultiCallback(onLoad);
-				var introComplete = callbacks.add("introComplete");
-				checkLoadSong(getSongPath());
-				if (PlayState.SONG.needsVoices)
-					checkLoadSong(getVocalPath());
-				checkLibrary("shared");
-                                FlxGraphic.defaultPersist = false;
-                                setLoadingText("Done!");
-				var fadeTime = 0.5;
-				FlxG.camera.fade(FlxG.camera.bgColor, fadeTime, true);
-				new FlxTimer().start(fadeTime + MIN_TIME, function(_) introComplete());
-			}
-		);
-	}
-	
-	function checkLoadSong(path:String)
-	{
-		if (!Assets.cache.hasSound(path))
-		{
-			var library = Assets.getLibrary("songs");
-			final symbolPath = path.split(":").pop();
-			// @:privateAccess
-			// library.types.set(symbolPath, SOUND);
-			// @:privateAccess
-			// library.pathGroups.set(symbolPath, [library.__cacheBreak(symbolPath)]);
-			var callback = callbacks.add("song:" + path);
-			Assets.loadSound(path).onComplete(function (_) { callback(); });
-		}
-	}
-	
-	function checkLibrary(library:String)
-	{
-		trace(Assets.hasLibrary(library));
-		if (Assets.getLibrary(library) == null)
-		{
-			@:privateAccess
-			if (!LimeAssets.libraryPaths.exists(library))
-				throw "Missing library: " + library;
-			
-			var callback = callbacks.add("library:" + library);
-			Assets.loadLibrary(library).onComplete(function (_) { callback(); });
-		}
-	}
-	
-	override function beatHit()
-	{
-		super.beatHit();
-	}
-	
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-                var lerpTarget:Float = 1280.0 * (progress / max);
+		var lerpTarget:Float = 1280.0 * (progress / max);
 		loadTxtProgress.scale.x = FlxMath.lerp(loadTxtProgress.scale.x, lerpTarget, elapsed * 5);
-
-		#if debug
-		if (FlxG.keys.justPressed.SPACE)
-			trace('fired: ' + callbacks.getFired() + " unfired:" + callbacks.getUnfired());
-		#end
 	}
 
-        public function setLoadingText(text:String)
+	public function setLoadingText(text:String)
 	{
 		loadTxt.text = text;
+	}
+}
+
+class LoadingState extends MusicBeatState
+{
+	public static var target:FlxState;
+	public static var stopMusic = false;
+	static var imagesToCache:Array<String> = [];
+	//static var soundsToCache:Array<String> = [];
+
+	var screen:LoadingScreen;
+
+	public function new()
+	{
+		super();
+
+		FlxTransitionableState.skipNextTransIn = true;
+		FlxTransitionableState.skipNextTransOut = true;
+	}
+
+	override function create()
+	{
+		super.create();
+
+		screen = new LoadingScreen();
+		add(screen);
+
+		screen.max = imagesToCache.length;
+
+		FlxG.camera.fade(FlxG.camera.bgColor, 0.5, true);
+
+		FlxGraphic.defaultPersist = true;
+		Thread.create(() ->
+		{
+			screen.setLoadingText("Loading images...");
+			for (image in imagesToCache)
+			{
+				trace("Caching image " + image);
+				FlxG.bitmap.add(Paths.image(image));
+				screen.progress += 1;
+			}
+
+			FlxGraphic.defaultPersist = false;
+
+			screen.setLoadingText("Done!");
+			trace("Done caching");
+
+			FlxG.camera.fade(FlxColor.BLACK, 1, false);
+			new FlxTimer().start(1, function(_:FlxTimer)
+			{
+				screen.kill();
+				screen.destroy();
+				loadAndSwitchState(target, false);
+			});
+		});
 	}
 	
 	function onLoad()
@@ -203,18 +170,15 @@ class LoadingState extends MusicBeatState
 	{
 		return Paths.voices(PlayState.SONG.song);
 	}
-	
-	inline static public function loadAndSwitchState(target:FlxState, stopMusic = false)
+
+	public static function loadAndSwitchState(target:FlxState, stopMusic = false)
 	{
 		FlxG.switchState(getNextState(target, stopMusic));
 	}
 	
 	static function getNextState(target:FlxState, stopMusic = false):FlxState
 	{
-		Paths.setCurrentLevel("week" + PlayState.storyWeek);
-		var loaded = isSoundLoaded(getSongPath())
-			&& (!PlayState.SONG.needsVoices || isSoundLoaded(getVocalPath()))
-			&& isLibraryLoaded("shared");
+		var loaded = isSoundLoaded(getSongPath()) && (!PlayState.SONG.needsVoices || isSoundLoaded(getVocalPath()));
 		
 		if (!loaded)
 			return new LoadingState(target, stopMusic);
@@ -228,146 +192,4 @@ class LoadingState extends MusicBeatState
 	{
 		return Assets.cache.hasSound(path);
 	}
-	
-	static function isLibraryLoaded(library:String):Bool
-	{
-		return Assets.getLibrary(library) != null;
-        }
-	
-	override function destroy()
-	{
-                pano = null;
-                panoclone = null;
-                logoBl = null;
-                loadTxtBg = null;
-	        loadTxtProgress = null;
-	        loadTxt = null;
-
-		super.destroy();
-		
-		callbacks = null;
-	}
-	
-	static function initSongsManifest()
-	{
-		var id = "songs";
-		var promise = new Promise<AssetLibrary>();
-
-		var library = LimeAssets.getLibrary(id);
-
-		if (library != null)
-		{
-			return Future.withValue(library);
-		}
-
-		var path = id;
-		var rootPath = null;
-
-		@:privateAccess
-		var libraryPaths = LimeAssets.libraryPaths;
-		if (libraryPaths.exists(id))
-		{
-			path = libraryPaths[id];
-			rootPath = Path.directory(path);
-		}
-		else
-		{
-			if (StringTools.endsWith(path, ".bundle"))
-			{
-				rootPath = path;
-				path += "/library.json";
-			}
-			else
-			{
-				rootPath = Path.directory(path);
-			}
-			@:privateAccess
-			path = LimeAssets.__cacheBreak(path);
-		}
-
-		AssetManifest.loadFromFile(path, rootPath).onComplete(function(manifest)
-		{
-			if (manifest == null)
-			{
-				promise.error("Cannot parse asset manifest for library \"" + id + "\"");
-				return;
-			}
-
-			var library = AssetLibrary.fromManifest(manifest);
-
-			if (library == null)
-			{
-				promise.error("Cannot open library \"" + id + "\"");
-			}
-			else
-			{
-				@:privateAccess
-				LimeAssets.libraries.set(id, library);
-				library.onChange.add(LimeAssets.onChange.dispatch);
-				promise.completeWith(Future.withValue(library));
-			}
-		}).onError(function(_)
-		{
-			promise.error("There is no asset library with an ID of \"" + id + "\"");
-		});
-
-		return promise.future;
-	}
-}
-
-class MultiCallback
-{
-	public var callback:Void->Void;
-	public var logId:String = null;
-	public var length(default, null) = 0;
-	public var numRemaining(default, null) = 0;
-	
-	var unfired = new Map<String, Void->Void>();
-	var fired = new Array<String>();
-	
-	public function new (callback:Void->Void, logId:String = null)
-	{
-		this.callback = callback;
-		this.logId = logId;
-	}
-	
-	public function add(id = "untitled")
-	{
-		id = '$length:$id';
-		length++;
-		numRemaining++;
-		var func:Void->Void = null;
-		func = function ()
-		{
-			if (unfired.exists(id))
-			{
-				unfired.remove(id);
-				fired.push(id);
-				numRemaining--;
-				
-				if (logId != null)
-					log('fired $id, $numRemaining remaining');
-				
-				if (numRemaining == 0)
-				{
-					if (logId != null)
-						log('all callbacks fired');
-					callback();
-				}
-			}
-			else
-				log('already fired $id');
-		}
-		unfired[id] = func;
-		return func;
-	}
-	
-	inline function log(msg):Void
-	{
-		if (logId != null)
-			trace('$logId: $msg');
-	}
-	
-	public function getFired() return fired.copy();
-	public function getUnfired() return [for (id in unfired.keys()) id];
 }
